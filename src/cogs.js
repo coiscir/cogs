0034130c80e444f100e0d374386580fd334fca41
Jonathan Lonowski <coiscir@gmail.com>
2009-04-24 14:25:03 -0500

    Setting up build environment.

---
+++ src/cogs.js
@@ -0,0 +1,8 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Cogs JavaScript Library, Alpha
+ *  (c) 2008-2009 Jonathan Lonowski
+ *
+ *  http://github.com/coiscir/cogs/
+ *
+ *  Cogs is released and distributable under the terms of the MIT License.
+***~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**/
