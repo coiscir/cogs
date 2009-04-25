5817606266a2a5c7789b7de535d21b225a20c2c8
Jonathan Lonowski <coiscir@gmail.com>
2009-04-24 20:20:47 -0500

    Added `serialize` to Query.

--- src/query.js
+++ src/query.js
@@ -19,4 +19,32 @@
     return matches;
   };
   
+  $C.serialize = function (form) {
+    var serial = [], add = function (key, value) {
+      serial.push((
+        encodeURIComponent(key) + '=' + encodeURIComponent(value)
+      ).replace(/%20/g, '+'));
+    };
+    
+    for (var i = 0, j, element; i < form.elements.length; i += 1) {
+      element = form.elements[i];
+      if (element.name.length > 0 && !element.disabled) {
+        switch (element.type) {
+          case 'file': break;
+          case 'select-one': add(element.name, element.value); break;
+          case 'select-multiple':
+            for (j = 0; j < element.options.length; j += 1)
+              if (element.options[j].selected && !element.options[j].disabled)
+                add(element.name, element.options[j].value);
+            break;
+          case 'checkbox':
+          case 'radio': if (!element.checked) break;
+          default: add(element.name, element.value); break;
+        }
+      }
+    }
+    
+    return serial.join('&');
+  };
+  
 })();
