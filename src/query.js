d7488c418a67ef9fbef310895fc334fb6ec72294
Jonathan Lonowski <coiscir@gmail.com>
2009-04-24 19:10:00 -0500

    Revised variable names and error messages.

--- src/query.js
+++ src/query.js
@@ -4,16 +4,16 @@
 
 (function Query() {
   
-  $C.query = function (key) {
-    if (!$C.is_a(key, String))
-      throw new Error('Invalid argument. Expected: String key.');
+  $C.query = function (search) {
+    if (!$C.is_a(search, String))
+      throw new Error('Search must be a String.');
     
-    var qstring = window.location.search.replace(/^\?/, '');
-    var queries = qstring.replace(/\+/g, '%20').split(/\&/);
+    var query = window.location.search.replace(/^\?/, '');
+    var pairs = query.replace(/\+/g, '%20').split(/\&/);
     
-    for (var i = 0, pair, matches = []; i < queries.length; i += 1) {
-      pair = queries[i].split(/=/, 2);
-      if (key === decodeURIComponent(pair[0]))
+    for (var i = 0, pair, matches = []; i < pairs.length; i += 1) {
+      pair = pairs[i].split(/=/, 2);
+      if (search === decodeURIComponent(pair[0]))
         matches.push(decodeURIComponent(pair[1] || ''));
     }
     return matches;
