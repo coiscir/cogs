c2761a06aa5d38ebcfbd929006ad3af6c04b516d
Jonathan Lonowski <coiscir@gmail.com>
2010-04-23 00:28:50 -0500

    Add Cogs.type, a substitute for typeof.

--- cogs-min.js
+++ cogs-min.js
@@ -6,4 +6,4 @@
  *  Released and distributed under the MIT License.
 **/
 
-"use strict";new function Cogs(){var a=window.Cogs=this;a.fn=a.constructor.prototype}();
+"use strict";new function Cogs(){var b=window.Cogs=this;b.fn=b.constructor.prototype;(function(){var f='undefined',j='null',k='array',c='boolean',l='date',m='error',g='function',d='number',h='object',i='regexp',e='string',n='unknown';b.fn.type=function o(a){switch(typeof a){case f:return f;case c:return c;case d:return d;case e:return e;case g:if(a.constructor==RegExp)return i;return g;case h:if(a===null)return j;if(a instanceof Error)return m;switch(a.constructor){case Array:return k;case Boolean:return c;case Date:return l;case Number:return d;case RegExp:return i;case String:return e;default:return h}default:return n}}})()}();
--- cogs.js
+++ cogs.js
@@ -11,4 +11,45 @@
 new function Cogs() {
   var Cogs = window.Cogs = this;
   Cogs.fn = Cogs.constructor.prototype;
+  
+  
+  /**************/
+  /** Type Cog **/
+  /**************/
+  
+  (function () {
+    
+    var UND = 'undefined', NUL = 'null',
+        ARR = 'array',     BLN = 'boolean',
+        DTE = 'date',      ERR = 'error',
+        FNC = 'function',  NUM = 'number',
+        OBJ = 'object',    RGX = 'regexp',
+        STR = 'string',    UNK = 'unknown';
+    
+    Cogs.fn.type = function type(object) {
+      switch (typeof object) {
+        case UND: return UND;
+        case BLN: return BLN;
+        case NUM: return NUM;
+        case STR: return STR;
+        case FNC:
+          if (object.constructor == RegExp) return RGX;
+          return FNC;
+        case OBJ:
+          if (object === null) return NUL;
+          if (object instanceof Error) return ERR;
+          switch (object.constructor) {
+            case Array:   return ARR;
+            case Boolean: return BLN;
+            case Date:    return DTE;
+            case Number:  return NUM;
+            case RegExp:  return RGX;
+            case String:  return STR;
+            default:      return OBJ;
+          }
+        default: return UNK;
+      }
+    };
+    
+  })();
 }();
