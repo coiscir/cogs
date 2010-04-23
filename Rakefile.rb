e806cc28c2d8d56ad7f8e9c4239476c51b2ea36b
Jonathan Lonowski <coiscir@gmail.com>
2010-04-22 23:11:09 -0500

    Move test suite in prep for replacement.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,33 +1,22 @@
-Dir.chdir(File.expand_path(File.dirname(__FILE__)))
-
+require 'rubygems'
 require 'rake'
-require 'build/builder'
+require 'packr'
 
-CGOS_BUILD = Time.now.utc.freeze
-COGS_VERSION = CGOS_BUILD.strftime('%Y.%m%d')
+@root = File.dirname(__FILE__)
+@dev = File.join(@root, "cogs.js")
+@min = File.join(@root, "cogs-min.js")
 
-LIB = File.join('lib', 'cogs.js')
-MIN = File.join('lib', 'cogs.min.js')
-SRC = File.join('src', 'cogs.js')
-
-task :default => :build
-
-task :leadin do
-  print $/
-end
-
-task :build => :leadin do
-  print 'Build :: ' + COGS_VERSION + $/
-  File.open(LIB, 'w+b') {|out| out << Builder.build(SRC, false)}
-  File.open(MIN, 'w+b') {|out| out << Builder.build(SRC, true)}
-  print ' + ' + LIB + $/ if File.exists?(LIB)
-  print ' + ' + MIN + $/ if File.exists?(MIN)
-  print $/
-end
-
-task :clean => :leadin do
-  print 'Clean' + $/
-  print ' - ' + LIB + $/ if File.exists?(LIB) && File.delete(LIB) > 0
-  print ' - ' + MIN + $/ if File.exists?(MIN) && File.delete(MIN) > 0
-  print $/
+task :default do
+  options = {
+    :shrink_vars => true
+  }
+  
+  source = File.read(@dev)
+  comment = (source.match(/\/\*\*!.*?\*\*\//m) or [''])[0].sub('/**!', '/**')
+  
+  File.open(@min, 'w+b') do |out|
+    out << comment + $/ + $/ + Packr.pack(source, options).strip + $/
+  end
+  
+  print ' + ' + @min.sub(@root, '.') + $/
 end
---
+++ cogs-min.js
@@ -0,0 +1,9 @@
+/**
+ *  Cogs JavaScript Library
+ *  http://github.com/coiscir/cogs/
+ *
+ *  Copyright (c) 2009-2010 Jonathan Lonowski
+ *  Released and distributed under the MIT License.
+**/
+
+"use strict";new function Cogs(){var a=window.Cogs=this;a.fn=a.constructor.prototype}();
