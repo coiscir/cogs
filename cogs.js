b3304515f7157ef0221556f559ad71707f60ec97
Jonathan Lonowski <coiscir@gmail.com>
2010-04-22 22:50:41 -0500

    Add initial unified file.

---
+++ cogs.js
@@ -0,0 +1,14 @@
+/**!
+ *  Cogs JavaScript Library
+ *  http://github.com/coiscir/cogs/
+ *
+ *  Copyright (c) 2009-2010 Jonathan Lonowski
+ *  Released and distributed under the MIT License.
+**/
+
+"use strict";
+
+new function Cogs() {
+  var Cogs = window.Cogs = this;
+  Cogs.fn = Cogs.constructor.prototype;
+}();
